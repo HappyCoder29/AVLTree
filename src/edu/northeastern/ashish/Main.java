9702dd94d8130990acb91e15dd3fe622aa57c08a
Ashish <happycoder29@gmail.com>
2020-03-19 19:52:34 -0700

    Initial commit

---
+++ src/edu/northeastern/ashish/Main.java
@@ -0,0 +1,19 @@
+package edu.northeastern.ashish;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        AVL tree = new AVL();
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(3);
+        tree.insert(4);
+        tree.insert(5);
+        tree.insert(6);
+        tree.insert(7);
+        tree.insert(10);
+
+        tree.levelOrder();
+    }
+}
